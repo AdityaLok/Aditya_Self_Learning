b8e83d07cc51786beeccc6bf4318bd29c49c00b6
AdityaLok <aditya.lokhande@globallogic.com>
2019-02-13 21:12:01 +0530

    Second module complete

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,19 +1,59 @@
 package com.example.project;
 
+
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private final String TAG = MainActivity.class.getName();
+    private Button button1;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
         Log.d(TAG, "onCreate");
+        button1 = findViewById(R.id.btnButton1);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"onStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"onResume");
+    }
+
+    public void onButton1ClickListener(View view) {
+                Toast.makeText(MainActivity.this, button1.getText(),Toast.LENGTH_SHORT).show();
+                Log.d(TAG,"onClick");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"onDestroy");
+    }
 }
